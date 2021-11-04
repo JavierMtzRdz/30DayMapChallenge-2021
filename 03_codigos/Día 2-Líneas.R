24fffc9c4573f0a6531759da5d3b65005e114a99
Javier Martinez Rodriguez <javierwolfsangel@gmail.com>
2021-11-04 17:39:45 -0600

    30DMC: Día 3-Polígonos

--- 03_codigos/Día 2-Líneas.R
+++ 03_codigos/Día 2-Líneas.R
@@ -113,10 +113,10 @@ ggsave(paste0("02_graficas/",
 map_oaxaca_cont <- st_read("01_datos_brutos/shp estatal/ent_simp.shp") %>% 
   filter(nom_ent == "Oaxaca") 
 
-polig <- st_coordinates(map_oaxaca_cont) %>%
-  data.frame() %>%
-  transmute(x_lon = X,
-            y_lat = Y)
+# polig <- st_coordinates(map_oaxaca_cont) %>%
+#   data.frame() %>%
+#   transmute(x_lon = X,
+#             y_lat = Y)
 
 no_oax <- st_read("01_datos_brutos/shp estatal/ent_simp.shp") %>% 
   filter(nom_ent != "Oaxaca") 
@@ -133,6 +133,7 @@ list_rios <- st_intersects(rios,
 
 rios_oax <- rios[list_rios[,1],]
 
+
 ## Generar data frame de montañas más altas ----
 
 principales_montanas_oax <- tibble(nombre = c("Cerro Nube (Quie Yelaag)",
@@ -140,6 +141,11 @@ principales_montanas_oax <- tibble(nombre = c("Cerro Nube (Quie Yelaag)",
                                lat = c(16.2115, 16.2667),
                                lon = c(-96.1967, -96.2833),
                                elevation = c(3720, 3640))
+
+polig <- tibble(x_lon = c(-96.3967, -96.4833,
+                          -95.9967, -96.0833),
+                y_lat = c(16.4115, 16.4667,
+                          16.0115, 16.0667))
 
 ## Generar mapa de Oaxaca ----
 
@@ -157,10 +163,14 @@ ggplot() +
   geom_sf(data = no_oax,
           fill = "grey97",
           size = 0.20) +
-  coord_sf(ylim = c(min(polig$y_lat), 
-                    max(polig$y_lat)),
-           xlim = c(min(polig$x_lon), 
-                    max(polig$x_lon))) +
+  ylim(c(min(polig$y_lat), 
+         max(polig$y_lat))) +
+  xlim(c(min(polig$x_lon), 
+         max(polig$x_lon))) +
+  # coord_sf(ylim = c(min(polig$y_lat), 
+  #                   max(polig$y_lat)),
+  #          xlim = c(min(polig$x_lon), 
+  #                   max(polig$x_lon))) +
   geom_point(principales_montanas_oax,
              mapping = aes(x = lon,
                            y = lat),
@@ -206,6 +216,93 @@ ggplot() +
 
 ggsave(paste0("02_graficas/",
               "30DMC-Día-2-Líneas-Oaxaca",
+              ".png"),
+       bg = "transparent",
+       width = 200,                  # Ancho de la gráfica
+       height = 110,
+       units = "mm")
+
+
+# Mapa de Oaxaca con acercamiento a montañas----
+# Generar límites -----
+dist <- 0.5
+
+polig_zoom <- tibble(x_lon = c(-96.2833 - dist - 0.3,
+                               -96.1967 + dist + 0.3),
+                     y_lat = c(+16.2667 + dist,
+                               +16.2115 - dist))
+
+## Generar mapa de Oaxaca ----
+
+ggplot() +
+  geom_sf(data = map_oaxaca_cont,
+          fill = "grey93",
+          size = 0.20) +
+  geom_sf(data = curvas_oax,
+          mapping = aes(color = contour,
+                        alpha = contour),
+          size = 0.20) +
+  geom_sf(data = rios_oax,
+          color = '#0d47a1',
+          alpha = 0.8,
+          size = 0.30) +
+  geom_sf(data = no_oax,
+          fill = "grey97",
+          size = 0.20) +
+  ylim(c(min(polig_zoom$y_lat), 
+         max(polig_zoom$y_lat))) +
+  xlim(c(min(polig_zoom$x_lon), 
+         max(polig_zoom$x_lon))) +
+  # coord_sf(ylim = c(min(polig_zoom$y_lat), 
+  #                   max(polig_zoom$y_lat)),
+  #          xlim = c(min(polig_zoom$x_lon), 
+  #                   max(polig_zoom$x_lon))) +
+  geom_point(principales_montanas_oax,
+             mapping = aes(x = lon,
+                           y = lat),
+             color = "red",
+             size = 0.25) +
+  geom_text_repel(principales_montanas_oax,
+                  mapping = aes(x = lon,
+                                y = lat,
+                                label = paste0(nombre, "\n(",
+                                               comma(elevation), " m)")),
+                  color = "black",
+                  min.segment.length = 0,
+                  segment.curvature = -0.2,
+                  arrow = arrow(length = unit(0.015, "npc"),
+                                # type = "closed",
+                                ends = "last"),
+                  force = 40,
+                  size = 3,
+                  segment.size = 0.4,
+                  segment.color = "grey25",
+                  bg.color = "grey90",
+                  bg.r = 0.1,
+                  fontface = "bold") +
+  theme_void() +
+  theme(legend.position = c(0.85, 0.75),
+        panel.background = element_rect(fill = "grey97",
+                                        color = "transparent")) +
+  scale_color_gradient(low= "grey65",
+                       high = '#931323',
+                       na.value = "white",
+                       name='Altitud (mts)',
+                       labels = comma,
+                       trans = "log",
+                       guide = guide_colourbar(direction = "horizontal",
+                                               barheight = unit(3, 
+                                                                units = "mm"),
+                                               barwidth = unit(40, 
+                                                               units = "mm"),
+                                               draw.ulim = F,
+                                               title.position = 'top',
+                                               label.position = "bottom",
+                                               title.hjust = 0.5,
+                                               label.hjust = 0.5))
+
+ggsave(paste0("02_graficas/",
+              "30DMC-Día-2-Líneas-Oaxaca-zoom",
               ".png"),
        bg = "transparent",
        width = 200,                  # Ancho de la gráfica
---
+++ 03_codigos/Día 3-Polígonos.R
@@ -0,0 +1,199 @@
+##################################################################
+##       Proyecto: 30DayMapChallenge 2021: Día 3-Polígonos      ##
+##################################################################
+##
+## Descripción:    Este script con las curvas de nivel y ríos de
+##                 México
+##
+## Autor:          Javier Mtz.  
+##
+## Fecha creac.:   2021-11-01
+##
+## Email:          javier.mtz.rd@gmail.com
+##
+## ---------------------------
+## Notas:          
+## ---------------------------
+
+# Setup ----
+## Paquetes a utilizar ----
+pacman::p_load(tidyverse, janitor, writexl, readxl, scales, mexicoR,
+               sf, rgdal, ggrepel, naniar, hablar, lwgeom)
+
+## Especificar locale ----
+Sys.setlocale("LC_ALL", "es_ES.UTF-8")
+
+## Desabilitar notación científica.----
+options(scipen = 999)
+
+# Cargar datos ----
+## Cargar distritos electorales ----
+distritos <- st_read("01_datos_brutos/electoral_2020/distritos_simp.shp") %>%
+  clean_names() %>%
+  st_transform("+init=epsg:4326") %>% 
+  sf::st_make_valid()
+
+## Generar capa con shapefile de la entidad ----
+entidad <- distritos %>% 
+  count(entidad)
+
+## Cargar datos electorales por distrito ----
+res_comp_dist_2021 <- read_excel("04_datos_generados/res_comp_dist_2021.xlsx")
+
+# Cargar datos de localidades amanzanadas ----
+
+localidades <- st_read("01_datos_brutos/loc 2020/00l.shp") %>% 
+  clean_names() %>% 
+  st_transform("+init=epsg:4326") %>% 
+  sf::st_make_valid()
+
+sf::sf_use_s2(FALSE)
+
+localidades$geometry <- localidades$geometry %>%
+  s2::s2_rebuild() %>%
+  sf::st_as_sfc()
+
+# Crusar localidades urbanos con distritos ----
+
+distritos_loc <- st_join(localidades, distritos) %>% 
+  count(entidad, distrito_f)
+
+# Datos geográficos con computos 2021 ----
+## Datos con mapa dacimétrico ----
+distritos_21_urb <- distritos_loc %>%
+  full_join(res_comp_dist_2021 %>%
+              filter(ganador == "Ganador"),
+            by = c("entidad" = "id_estado",
+                   "distrito_f" = "id_distrito"))
+
+## Datos con mapa cloroplectico ----
+distritos_21 <- distritos %>%
+  full_join(res_comp_dist_2021 %>%
+              filter(ganador == "Ganador"),
+            by = c("entidad" = "id_estado",
+                   "distrito_f" = "id_distrito"))
+
+
+# Generar mapa nacional dacimétrico -----
+distritos_21 %>% 
+  ggplot() +
+  geom_sf(data = distritos_21_urb,
+          mapping = aes(fill = participación),
+          size = 0.01,
+          color = "grey55") +
+  geom_sf(aes(fill = participación),
+          size = 0.2,
+          alpha = 0.1,
+          color = "grey35") +
+  geom_sf(data = entidad,
+          fill = "transparent",
+          size = 0.3,
+          color = "grey15") +
+  theme_void() +
+  theme(legend.position = c(0.85, 0.75),
+        panel.background = element_rect(fill = "grey97",
+                                        color = "transparent")) +
+  scale_fill_gradient(low = "grey75",
+                      high = '#0e9a9d',
+                      na.value = "white",
+                      name = 'Participación \nelectoral 2021',
+                      labels = percent_format(accuracy = 1L),
+                      # trans = "log",
+                      guide = guide_colourbar(direction = "horizontal",
+                                              barheight = unit(3, 
+                                                               units = "mm"),
+                                              barwidth = unit(40, 
+                                                              units = "mm"),
+                                              draw.ulim = F,
+                                              title.position = 'top',
+                                              label.position = "bottom",
+                                              title.hjust = 0.5,
+                                              label.hjust = 0.5))
+
+
+# Generar mapa dacimétrico del centro de México ----
+distritos_21 %>% 
+  ggplot() +
+  geom_sf(data = distritos_21_urb,
+          mapping = aes(fill = participación),
+          size = 0.01,
+          color = "grey55") +
+  geom_sf(aes(fill = participación),
+          size = 0.2,
+          alpha = 0.1,
+          color = "grey35") +
+  geom_sf(data = entidad,
+          fill = "transparent",
+          size = 0.3,
+          color = "grey15") +
+  ylim(c(15.8, 20)) +
+  xlim(c(-100.8, -94.2)) +
+  theme_void() +
+  theme(legend.position = c(0.85, 0.8),
+        panel.background = element_rect(fill = "grey97",
+                                        color = "transparent")) +
+  scale_fill_gradient(low = "grey75",
+                      high = '#0e9a9d',
+                      na.value = "white",
+                      name = 'Participación \nelectoral 2021',
+                      labels = percent_format(accuracy = 1L),
+                      # trans = "log",
+                      guide = guide_colourbar(direction = "horizontal",
+                                              barheight = unit(3, 
+                                                               units = "mm"),
+                                              barwidth = unit(40, 
+                                                              units = "mm"),
+                                              draw.ulim = F,
+                                              title.position = 'top',
+                                              label.position = "bottom",
+                                              title.hjust = 0.5,
+                                              label.hjust = 0.5))
+
+ggsave(paste0("02_graficas/",
+              "30DMC-Día-3-Polígono-Dasimétrico-CentroMx",
+              ".png"),
+       bg = "transparent",
+       width = 200,                  # Ancho de la gráfica
+       height = 110,
+       units = "mm")
+
+# Generar mapa cloropléctico del centro de México ----
+distritos_21 %>% 
+  ggplot() +
+  geom_sf(aes(fill = participación),
+          size = 0.2,
+          color = "grey35") +
+  geom_sf(data = entidad,
+          fill = "transparent",
+          size = 0.3,
+          color = "grey15") +
+  ylim(c(15.8, 20)) +
+  xlim(c(-100.8, -94.2)) +
+  theme_void() +
+  theme(legend.position = c(0.85, 0.8),
+        panel.background = element_rect(fill = "grey97",
+                                        color = "transparent")) +
+  scale_fill_gradient(low = "grey75",
+                      high = '#0e9a9d',
+                      na.value = "white",
+                      name = 'Participación \nelectoral 2021',
+                      labels = percent_format(accuracy = 1L),
+                      # trans = "log",
+                      guide = guide_colourbar(direction = "horizontal",
+                                              barheight = unit(3, 
+                                                               units = "mm"),
+                                              barwidth = unit(40, 
+                                                              units = "mm"),
+                                              draw.ulim = F,
+                                              title.position = 'top',
+                                              label.position = "bottom",
+                                              title.hjust = 0.5,
+                                              label.hjust = 0.5))
+
+ggsave(paste0("02_graficas/",
+              "30DMC-Día-3-Polígono-Cloropléctico-CentroMx",
+              ".png"),
+       bg = "transparent",
+       width = 200,                  # Ancho de la gráfica
+       height = 110,
+       units = "mm")
